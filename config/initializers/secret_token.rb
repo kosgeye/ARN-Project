414fe549b1552c73bbb77da8db48942f40b6e9cb
Eric K. Kosgey <kosgeye@berea.edu>
2012-02-15 13:39:31 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Farm::Application.config.secret_token = '7c0d39e2f9ad00ff0c29a776ea1b65a8cd5155167982e2f06fa490f41ffe4ef48c7ed136b68a1caa5e817371c6453a3819be89f1a1e47e61cdf793261dda56c0'
